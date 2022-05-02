ce686e2fb5f335a1ed30fcff995d235d7c40df4a
polina <93357529+fullmetalemo@users.noreply.github.com>
2022-05-02 21:12:01 +0300

    Update Running.java

--- MainBody/app/Running.java
+++ MainBody/app/Running.java
@@ -7,28 +7,33 @@ import utilities.*;
 public class Running{
   
   public static void main(String[] args){
-    try (Scanner newScanner = new Scanner){ //not finished
+    try (Scanner newScanner = new Scanner){
+      final String envVariable = "LAB";
+
+            PassportAsker passportAsker = new PassportAsker(userScanner);
+            FileManager fileManager = new FileManager(envVariable);
+            CollectionManager collectionManager = new CollectionManager(fileManager);
     
-    Commands( //not finished
-      new Help(),
-      new Info(),
-      new Show(),
-      new Add(),
-      new UpdateID(),
-      new RemoveByID(),
-      new Clear(),
-      new Save(),
-      new Exit(),
-      new ExecuteScript(),
-      new AddIfMax(),
-      new RemoveGreater(),
-      new History(),
-      new MaxByHeight(),
-      new FilterByNationality(),
-      new FilterGreaterThanBirthday()
-    );
-      
-    
+            CommandManager commandManager = new CommandManager( //not finished
+            new Help(),
+            new Info(),
+            new Show(),
+            new Add(),
+            new UpdateID(),
+            new RemoveByID(),
+            new Clear(),
+            new Save(),
+            new Exit(),
+            new ExecuteScript(),
+            new AddIfMax(),
+            new RemoveGreater(),
+            new History(),
+            new MaxByHeight(),
+            new FilterByNationality(),
+            new FilterGreaterThanBirthday()
+          );
+          Console console = new Console(commandManager, userScanner, marineAsker);
+          console.interactiveMode();
     }
   }
 }
